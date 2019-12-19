6140a2fb572d034fb6a39df87ef39af188f4e73a
haiyangxue <xue.haihang@gmail.com>
2019-12-19 17:17:07 +0800

    add audio feature

--- interactive.py
+++ interactive.py
@@ -11,10 +11,11 @@ from collections import namedtuple
 import fileinput
 
 import torch
-
+import os
 from fairseq import checkpoint_utils, options, tasks, utils
-from fairseq.data import encoders
-
+from fairseq.data import encoders,data_utils
+import torchaudio
+import torchaudio.compliance.kaldi as kaldi
 
 Batch = namedtuple('Batch', 'ids src_tokens src_lengths')
 Translation = namedtuple('Translation', 'src_str hypos pos_scores alignments')
@@ -67,8 +68,10 @@ def main(args):
     assert not args.max_sentences or args.max_sentences <= args.buffer_size, \
         '--max-sentences/--batch-size cannot be larger than --buffer-size'
 
-    print(args)
-
+    # print(args)
+    print()
+    print("*******************")
+    print(args.task)
     use_cuda = torch.cuda.is_available() and not args.cpu
 
     # Setup task, e.g., translation
@@ -118,6 +121,23 @@ def main(args):
             x = tokenizer.decode(x)
         return x
 
+    def collate_frames(frames):
+        """Convert a list of 2d frames into a padded 3d tensor
+        Args:
+            frames (list): list of 2d frames of size L[i]*f_dim. Where L[i] is
+                length of i-th frame and f_dim is static dimension of features
+        Returns:
+            3d tensor of size len(frames)*len_max*f_dim where len_max is max of L[i]
+        """
+        len_max = max(frame.size(0) for frame in frames)
+        print(frames.size())
+        print(frames[0].size())
+        f_dim = frames[0].size(1)
+        res = frames[0].new(len(frames), len_max, f_dim).fill_(0.0)
+        for i, v in enumerate(frames):
+            res[i, : v.size(0)] = v
+        return res
+
     # Load alignment dictionary for unknown word replacement
     # (None if no unknown word replacement, empty if no path to align dictionary)
     align_dict = utils.load_align_dict(args.replace_unk)
@@ -127,58 +147,95 @@ def main(args):
         *[model.max_positions() for model in models]
     )
 
-    if args.buffer_size > 1:
-        print('| Sentence buffer size:', args.buffer_size)
-    print('| Type the input sentence and press return:')
+    # if args.buffer_size > 1:
+    #     print('| Sentence buffer size:', args.buffer_size)
+    # print('| Type the input sentence and press return:')
     start_id = 0
-    for inputs in buffered_read(args.input, args.buffer_size):
-        results = []
-        for batch in make_batches(inputs, args, task, max_positions, encode_fn):
-            src_tokens = batch.src_tokens
-            src_lengths = batch.src_lengths
-            if use_cuda:
-                src_tokens = src_tokens.cuda()
-                src_lengths = src_lengths.cuda()
+    audio_root_path="/search/odin/haiyang/fairseq_exp/e2e_trans/fairseq/examples/speech_recognition/datasets/zh_asr_data/train/train2"
+    with open(args.input)as inp:
+        input = inp.readline().strip()
+        while input:
+            print()
+            audio_path=audio_root_path+"/"+input.split(" ")[0]+".wav"
+            inputs = [" ".join(input.split(" ")[1:])]
+            results = []
+            for batch in make_batches(inputs, args, task, max_positions, encode_fn):
+                src_tokens = batch.src_tokens
+                src_lengths = batch.src_lengths
+                if use_cuda:
+                    src_tokens = src_tokens.cuda()
+                    src_lengths = src_lengths.cuda()
+                if args.task=="translation":
+                    sample = {
+                        'net_input': {
+                            'src_tokens': src_tokens,
+                            'src_lengths': src_lengths,
+                        },
+                    }
+                else:
+                    if not os.path.exists(audio_path):
+                        raise FileNotFoundError("Audio file not found: {}".format(audio_path))
+                    sound, sample_rate = torchaudio.load_wav(audio_path)
+                    num_mel_bins ,frame_length,frame_shift= 80,25.0,10.0
 
-            sample = {
-                'net_input': {
-                    'src_tokens': src_tokens,
-                    'src_lengths': src_lengths,
-                },
-            }
-            translations = task.inference_step(generator, models, sample)
-            for i, (id, hypos) in enumerate(zip(batch.ids.tolist(), translations)):
-                src_tokens_i = utils.strip_pad(src_tokens[i], tgt_dict.pad())
-                results.append((start_id + id, src_tokens_i, hypos))
+                    output = kaldi.fbank(
+                        sound,
+                        num_mel_bins=num_mel_bins,
+                        frame_length=frame_length,
+                        frame_shift=frame_shift,
+                        dither=0.0,
+                        energy_floor=1.0
+                    )
 
-        # sort output to match input order
-        for id, src_tokens, hypos in sorted(results, key=lambda x: x[0]):
-            if src_dict is not None:
-                src_str = src_dict.string(src_tokens, args.remove_bpe)
-                print('S-{}\t{}'.format(id, src_str))
+                    frames = data_utils.apply_mv_norm(output).detach()[None,:,:].type(torch.cuda.FloatTensor)
+                    # print(output_cmvn)
+                    # frames = collate_frames(output_cmvn)
+                    # sort samples by descending number of frames
+                    # frames_lengths = torch.cuda.LongTensor(frames.size()[1])
+                    frames_lengths = torch.LongTensor([s.size(0) for s in frames])
 
-            # Process top predictions
-            for hypo in hypos[:min(len(hypos), args.nbest)]:
-                hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
-                    hypo_tokens=hypo['tokens'].int().cpu(),
-                    src_str=src_str,
-                    alignment=hypo['alignment'],
-                    align_dict=align_dict,
-                    tgt_dict=tgt_dict,
-                    remove_bpe=args.remove_bpe,
-                )
-                hypo_str = decode_fn(hypo_str)
-                print('H-{}\t{}\t{}'.format(id, hypo['score'], hypo_str))
-                print('P-{}\t{}'.format(
-                    id,
-                    ' '.join(map(lambda x: '{:.4f}'.format(x), hypo['positional_scores'].tolist()))
-                ))
-                if args.print_alignment:
-                    alignment_str = " ".join(["{}-{}".format(src, tgt) for src, tgt in alignment])
-                    print('A-{}\t{}'.format(
-                        id,
-                        alignment_str
-                    ))
+                    sample = {
+                    'net_input': {
+                        'src_tokens': src_tokens,
+                        'src_lengths': src_lengths,
+                        "audio": frames, "audio_lengths": frames_lengths
+                    },}
+                translations = task.inference_step(generator, models, sample)
+                for i, (id, hypos) in enumerate(zip(batch.ids.tolist(), translations)):
+                    src_tokens_i = utils.strip_pad(src_tokens[i], tgt_dict.pad())
+                    results.append((start_id + id, src_tokens_i, hypos))
+
+            # sort output to match input order
+            for id, src_tokens, hypos in sorted(results, key=lambda x: x[0]):
+                if src_dict is not None:
+                    src_str = src_dict.string(src_tokens, args.remove_bpe)
+                    print('S-{}\t{}'.format(id, src_str))
+
+                # Process top predictions
+                for hypo in hypos[:min(len(hypos), args.nbest)]:
+                    hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
+                        hypo_tokens=hypo['tokens'].int().cpu(),
+                        src_str=src_str,
+                        alignment=hypo['alignment'],
+                        align_dict=align_dict,
+                        tgt_dict=tgt_dict,
+                        remove_bpe=args.remove_bpe,
+                    )
+                    hypo_str = decode_fn(hypo_str)
+                    print('H-{}\t{}'.format(id, hypo_str))
+
+                    # print('H-{}\t{}\t{}'.format(id, hypo['score'], hypo_str))
+                    # print('P-{}\t{}'.format(
+                    #     id,
+                    #     ' '.join(map(lambda x: '{:.4f}'.format(x), hypo['positional_scores'].tolist()))
+                    # ))
+                    if args.print_alignment:
+                        alignment_str = " ".join(["{}-{}".format(src, tgt) for src, tgt in alignment])
+                        print('A-{}\t{}'.format(
+                            id,
+                            alignment_str
+                        ))
+            input = inp.readline().strip()
 
         # update running id counter
         start_id += len(inputs)
